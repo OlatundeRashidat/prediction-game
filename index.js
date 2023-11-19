00615056ded9e2a3f19e6688e0f2819cc7e44efb
famousconcept <famousconcept1@gmail.com>
2023-11-19 10:19:21 +0100

    javascript.js changed to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const random_number = Math.floor(Math.random()*10) + 1;
+const predict= prompt("predict a number from 1-10?");
+if(random_number===predict){
+    alert("congratulations, you win")
+} else{
+    alert("you loss, pls try again")
+}
+if (predict > 10){
+    alert("follow the instruction, your predict number shouldnt be greater than 10")
+} else{
+    alert("random_number")
+}
